56a4c92d0e0ccbebd79d00243ac65fb0c13c65bc
Jonthan Hutson <jhut110766@gmail.com>
2016-11-22 09:56:47 -0500

    sam-I-am

--- green_eggs_and_ham.rb
+++ green_eggs_and_ham.rb
@@ -14,7 +14,7 @@ class GreenEggsAndHam
     sorted_words = downcase_unique_words.sort
   end
 
-  def number_of_words_shorter_than (number)
+  def number_of_words_shorter_than (count)
     number_of_words = @words.select { |word| word.length < 4 }
     shorter_words = number_of_words.count
   end
@@ -28,13 +28,18 @@ class GreenEggsAndHam
     @text.split ("\n" * 2)
 
   end
+
   def lines
     @text.split("\n").reject { |word| word.empty? }
   end
-  def frequency_of_unique_words
-    downcase_words = @words.map { |word| word.downcase }
-    downcase_unique_words = downcase_words.uniq
-    sorted_words = downcase_unique_words.rindex
 
+  def frequency_of_unique_words
+    frequencies = Hash.new
+
+    sorted_unique_words.each do |unique_word|
+      frequency = @words.count { |word| word.downcase == unique_word }
+      frequencies[unique_word] = frequency
+    end
+    frequencies
   end
 end
